19c7128d40ce8c4090bf25ee84937edfedd2a6fa
kahdichienja <agooclinton@gmail.com>
2026-07-22 08:53:00 +0300

    k

--- documentverifier/settings.py
+++ documentverifier/settings.py
@@ -121,5 +121,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "verifier", "static")]
